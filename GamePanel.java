a6647b5a1c27c1a4af32212929ba0b1a210da67d
Googol <ronaldfleung@gmail.com>
2020-12-03 15:37:21 +0000

    create GamePanel constructor

--- GamePanel.java
+++ GamePanel.java
@@ -1,17 +1,32 @@
+import javax.swing.JPanel;
 import java.awt.Color;
+import java.awt.Graphics;
 
-public class GamePanel{
+public class GamePanel extends JPanel{
   // use this class to store constants about the Pong gameboard dimensions.
   public static final int WIDTH = 720;
   public static final int HEIGHT = 600;
   public static final int X_GAP = 50;
 
-  private int x;
-  private int y;
-  private int speed;
-  private int theta;
-  private Color color;
+  // colors
+  public static final Color LP_CLR = new Color(0, 255, 0);
+  public static final Color RP_CLR = new Color(0, 0, 255);
+  public static final Color B_CLR  = new Color(255, 0, 0);
 
+  // attributes
+  private Paddle leftPanel, rightPanel;
+  private Scoreboard leftScoreboard, rightScoreboard;
+  private Ball ball;
+
+  // constructor
+  public GamePanel() {
+    setSize(WIDTH, HEIGHT);
+    leftPanel = new Paddle(LP_CLR);
+    rightPanel = new Paddle(false, RP_CLR);
+    leftScoreboard = new Scoreboard(leftPanel);
+    rightScoreboard = new Scoreboard(rightPanel);
+    ball = new Ball(B_CLR);
+  }
 
   
 }
