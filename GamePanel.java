108905fb55622fc8f0e668e05b36b21a85eb6035
Googol <ronaldfleung@gmail.com>
2020-12-09 15:36:35 +0000

    add background and dashed line

--- GamePanel.java
+++ GamePanel.java
@@ -7,11 +7,15 @@ public class GamePanel extends JPanel{
   public static final int WIDTH = 720;
   public static final int HEIGHT = 600;
   public static final int X_GAP = 50;
+  public static final int Y_GAP = 20;
+  public static final int DASH_LEN = 18;
 
   // colors
   public static final Color LP_CLR = new Color(71, 71, 235);
   public static final Color RP_CLR = new Color(235, 71, 153);
   public static final Color B_CLR  = new Color(194, 71, 235);
+  public static final Color BG_CLR = new Color(54, 46, 56);
+  public static final Color LINE_CLR = new Color(108, 92, 112);
 
   // attributes
   private Paddle leftPaddle, rightPaddle;
@@ -21,6 +25,7 @@ public class GamePanel extends JPanel{
   // constructor
   public GamePanel() {
     setSize(WIDTH, HEIGHT);
+    setBackground(BG_CLR);
     leftPaddle = new Paddle(true, LP_CLR);
     rightPaddle = new Paddle(false, RP_CLR);
     leftScoreboard = new Scoreboard(leftPaddle);
@@ -30,6 +35,8 @@ public class GamePanel extends JPanel{
 
   @Override
   public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+	  drawBackground(g);
     leftPaddle.render(g); // call Graphics methods from Paddle class
     rightPaddle.render(g);
     ball.render(g);
@@ -37,4 +44,10 @@ public class GamePanel extends JPanel{
     rightScoreboard.render(g);
   }
   
+  private void drawBackground(Graphics g) {
+	  g.setColor(LINE_CLR);
+	  for(int y = Y_GAP; y < HEIGHT; y += 2*DASH_LEN) {
+		  g.drawLine(WIDTH/2, y, WIDTH/2, y+20);
+	  }
+  }
 }
